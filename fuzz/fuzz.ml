06ba6c9b5c347e1d46bc9b08f0a8f10c22776abd
Stephen Dolan <stephen.dolan@cl.cam.ac.uk>
2021-03-12 16:36:16 +0000

    better fuzzer

--- fuzz/fuzz.ml
+++ fuzz/fuzz.ml
@@ -4,6 +4,8 @@ open Lang.Exp
 
 let mkident s : ident = { label = s; shift = 0}, noloc
 
+let mkty t = (Some t, noloc)
+
 let tyexp = fix @@ fun tyexp ->
   let tfields =
     (* FIXME extend *)
@@ -32,7 +34,16 @@ let tyexp = fix @@ fun tyexp ->
 
     map [list bound; tyexp] (fun a b -> Tforall (a, b))
   ] in
-  map [tyexp'] @@ fun t -> (Some t, noloc)
+  map [tyexp'] mkty
+
+let tyexp = with_printer Typedefs.pp_tyexp tyexp
+
+let tyexp' =
+  let bound =
+    map [choose [const "A";const "B";const "C";const "D"]; option tyexp] @@ fun s t -> ((s,Exp.noloc), t) in
+  map [list1 bound; tyexp] (fun a b -> mkty (Tforall (a, b)))
+
+let tyexp' = with_printer Typedefs.pp_tyexp tyexp'
 
 let refl ts =
   match Lang.Check.typ_of_tyexp Env_nil ts with
@@ -43,5 +54,66 @@ let refl ts =
        Check.report e in
      Types.subtype ~error Env_nil t t
 
+let compare a b =
+  match Lang.Check.typ_of_tyexp Env_nil a, Lang.Check.typ_of_tyexp Env_nil b with
+  | exception _ -> bad_test ()
+  | a, b ->
+     try Types.subtype ~error:(fun _ -> raise Exit) Env_nil a b
+     with Exit | Typedefs.Unimplemented _ -> ()
+
+let parse t = Lang.Check.typ_of_tyexp Env_nil t
+
+let commute a b c d p q r s =
+  let pair x y = mkty (Trecord (Tuple_fields.(collect_fields [Fpos x; Fpos y]))) in
+  let lbound = [("A", noloc), a; ("B",noloc), b] in
+  let rbound = [("C", noloc), c; ("D",noloc), d] in
+  let tl = mkty (Tforall (lbound, pair p q)) in
+  let tr = mkty (Tforall (rbound, pair r s)) in
+
+  let tl, tr = try parse tl, parse tr with _ -> bad_test () in
+  let tl' = mkty (Tforall (lbound, pair q p)) in
+  let tr' = mkty (Tforall (rbound, pair s r)) in
+  let tl', tr' = parse tl', parse tr' in
+
+  let subt a b =
+    match Types.subtype ~error:(fun _ -> raise Exit) Env_nil a b with
+    | () -> `Yes
+    | exception Exit -> `No
+    | exception (Typedefs.Unimplemented _) -> `Unimp in
+  match subt tl tr, subt tl' tr' with
+  | `Unimp, _ | _, `Unimp -> ()
+  | s, t -> assert (s = t)
+
+let transitive a b c =
+  let a, b, c = try parse a, parse b, parse c with _ -> bad_test () in
+  let subt a b =
+    match Types.subtype ~error:(fun _ -> raise Exit) Env_nil a b with
+    | () -> true
+    | exception Exit -> false
+    | exception (Typedefs.Unimplemented _) -> bad_test () in
+  let ab = subt a b and bc = subt b c and ca = subt c a in
+  let ba = subt b a and cb = subt c b and ac = subt a c in
+  assert ((ab && bc) <= ac);
+  assert ((bc && ca) <= ba);
+  assert ((ca && ab) <= cb);
+  assert ((cb && ba) <= ca);
+  assert ((ac && cb) <= ab);
+  assert ((ba && ac) <= bc)
+  
+
+
 let () =
-  add_test ~name:"refl" [with_printer Typedefs.pp_tyexp tyexp] refl
+  add_test ~name:"refl" [tyexp'] refl
+
+(*
+let () =
+  add_test ~name:"compare" [tyexp; tyexp] compare
+
+let () =
+  add_test ~name:"commute" [option tyexp;option tyexp;option tyexp;option tyexp;tyexp;tyexp;tyexp;tyexp] commute
+*)
+
+(*
+let () =
+  add_test ~name:"trans" [tyexp'; tyexp'; tyexp'] transitive
+*)
